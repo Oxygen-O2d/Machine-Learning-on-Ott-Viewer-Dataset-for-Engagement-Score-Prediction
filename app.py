c11104fe74f4b32856eb0fd4125272f612711dc9
Oxygen-O2d <166587602+Oxygen-O2d@users.noreply.github.com>
2025-08-06 20:19:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import joblib
 
-# Load the trained model (saved using pickle)
 with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
+    model = joblib.load(f)
 
 # Define the features used in the model
-selected_features = [
-    'gender_Female', 'location_Bhopal', 'location_Coimbatore', 'location_Kolhapur', 'location_Nanded', 'location_Ujjain', 'language_preference_Assamese', 'language_preference_Bengali', 'language_preference_Hindi', 'language_preference_Malayalam', 'language_preference_Marathi', 'language_preference_Tamil', 'device_type_Laptop', 'device_type_Smartphone', 'device_type_Tablet', 'subscription_type_Free', 'content_genre_Comedy', 'content_genre_Crime', 'content_genre_Drama', 'content_genre_Horror-Comedy', 'content_genre_Romance', 'content_genre_Sci-Fi', 'content_genre_Thriller', 'content_language_Assamese', 'content_language_Gujarati', 'content_language_Kannada', 'content_language_Malayalam', 'content_language_Marathi', 'content_language_Odia', 'content_language_Punjabi', 'content_language_Tamil', 'Week_Day_Monday', 'Week_Day_Sunday', 'Season_Post-Monsoon', 'watch_time_hours', 'family_friendly_score', 'view_completion_rate', 'engagement_score', 'Year', 'Quarter'
-]
+selected_features = ['gender_Female', 'location_Bhopal', 'location_Coimbatore', 'location_Kolhapur', 'location_Nanded', 'location_Ujjain', 'language_preference_Assamese', 'language_preference_Bengali', 'language_preference_Hindi', 'language_preference_Malayalam', 'language_preference_Marathi', 'language_preference_Tamil', 'device_type_Laptop', 'device_type_Smartphone', 'device_type_Tablet', 'subscription_type_Free', 'content_genre_Comedy', 'content_genre_Crime', 'content_genre_Drama', 'content_genre_Horror-Comedy', 'content_genre_Romance', 'content_genre_Sci-Fi', 'content_genre_Thriller', 'content_language_Assamese', 'content_language_Gujarati', 'content_language_Kannada', 'content_language_Malayalam', 'content_language_Marathi', 'content_language_Odia', 'content_language_Punjabi', 'content_language_Tamil', 'Week_Day_Monday', 'Week_Day_Sunday', 'Season_Post-Monsoon', 'watch_time_hours', 'family_friendly_score', 'view_completion_rate', 'engagement_score', 'Year', 'Quarter']
 
 # Streamlit App UI
 st.title("📈 Engagement Score Predictions on an OTT platform")
